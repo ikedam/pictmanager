582b21b4720464ed8edeeebc626a1cd08b807503
ikedam <devld@ikedam.jp>
2023-05-06 16:51:23 +0900

    Support auto loading

--- client/src/app/image-list/image-list.component.ts
+++ client/src/app/image-list/image-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { Image } from 'src/app/model/image';
 import { ImageService } from 'src/app/service/image.service';
@@ -10,27 +10,51 @@ import { ImageService } from 'src/app/service/image.service';
 })
 export class ImageListComponent implements OnInit {
   imageList: Image[] = [];
-  hasMore = true
+  hasMore = true;
+  loading = false;
 
   constructor(private imageService: ImageService) {}
 
   ngOnInit() {
     const count = 20;
+    this.loading = true;
     this.imageService.getImageList(count).subscribe(imageList => {
+      this.loading = false;
       this.imageList = imageList;
       if (imageList.length < count) {
         this.hasMore = false
       }
+      this.checkNeedMore();
     });
   }
 
+  checkNeedMore() {
+    if (!this.hasMore) {
+      return;
+    }
+    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight - 100) {
+      this.onClickMore();
+    }
+  }
+
   onClickMore() {
+    if (this.loading) {
+      return;
+    }
     const count = 20;
+    this.loading = true;
     this.imageService.getImageList(count, this.imageList[this.imageList.length - 1]).subscribe(imageList => {
+      this.loading = false;
       this.imageList.push(...imageList);
       if (imageList.length < count) {
         this.hasMore = false
       }
+      this.checkNeedMore();
     });
+  }
+
+  @HostListener("window:scroll", [])
+  onScroll(): void {
+    this.checkNeedMore();
   }
 }
